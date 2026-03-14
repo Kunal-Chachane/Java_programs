4a4b3fff81718932f03f3fd8d71edb0f5e6d8786
kunal_chachane <kunalchachane59@gmail.com>
2026-03-14 20:43:56 +0530

    Add PowerOfTwo.java file

---
+++ PowerOfTwo.java
@@ -0,0 +1,29 @@
+
+// Given an integer n, return true if it is a power of two. Otherwise, return false.
+//An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+import java.util.Scanner;
+
+class num{
+    public static boolean isPowerOfTwo(int n) {
+        if (n <= 0){
+            return false;
+        }
+        while (n % 2 == 0) {
+            n /= 2;
+        }
+        return n == 1;
+    }
+
+}
+
+public class PowerOfTwo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+        boolean result = num.isPowerOfTwo(n);
+        System.out.println(result); 
+    }
+}
+
