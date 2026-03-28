1786e5be93cedf777e91822a91a574e20f27d40c
Kunal Chachane <kunalchachane59@gmail.com>
2026-03-28 21:30:50 +0530

    Program ofd Binary Search

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,31 +1,32 @@
 import java.util.Scanner;
-public class BubbleSort {
-  public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    System.out.print("Enter the number of elements: ");
-    int n = scanner.nextInt();
-    int[] arr = new int[n];
-    System.out.println("Enter the elements: ");
-    for (int i = 0; i < n; i++) {
-      arr[i] = scanner.nextInt();
-    }
-    BubbleSort(arr);
-    System.out.println("Sorted array: ");
-    for (int num : arr) {
-      System.out.print(num + " ");
-    }
-  }
 
-public static void BubbleSort(int[] arr) {
-  int n = arr.length;
-  for (int i = 0; i < n - 1; i++) {
-    for (int j = 0; j < n - 1 - i; j++) {
-      if (arr[j] > arr[j + 1]) {
-        int temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-      }
+public class BubbleSort{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array: ");
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for(int i=0;i<n;i++){
+        nums[i]= sc.nextInt();
     }
-  }
-}
+    bubbleSortLogic(nums);
+    System.out.println("Sorted array:");
+        for(int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+    public static void bubbleSortLogic(int[] nums){
+        int size = nums.length;
+        int temp = 0;
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size-1;j++){
+                if(nums[j] > nums[j+1]){
+                    temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
+        }
+        }
+    }
 }
